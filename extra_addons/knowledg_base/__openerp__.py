aa5e83d4d4e76a3b8c9873cd40091e39b54f013b
Evgenij Golosuev <golosuev@gmail.com>
2018-05-21 15:09:58 +0300

    new module

---
+++ extra_addons/knowledg_base/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name' : "Knowledg",
+    'version' : "8.0.1",
+    'website': 'simbioz.ua',
+    'author' : "Eugene Golosuev",
+    'depends' : ['base'],
+    'data' : ['views/knowledge.xml',
+              'views/knowledge_action.xml',
+              'views/knowledge_menu.xml'],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ extra_addons/knowledg_base/models/knowledge.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, tools, models, _
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+class KnowledgeBase(models.Model):
+    _name = 'knowledge.base'
+
+    name = fields.Char(string="Name")
+    date = fields.Date('Date Create', default=lambda self: fields.Datetime.now())
+    text_body = fields.Text(string="Text")
+    # section_ids = fields.One2many('section.section', string="Section Name")
+    general_concept = fields.Text(string='General Concept')
+    where_applicable = fields.Text(string='Where Applicable')
+    # information_url_ids = fields.One2many('sources.information', string="Sources of information")
+    literature = fields.Text(string="Literature")
+    code = fields.Char(string='Code')
+
+class SourcesOfInformation(models.Model):
+    _name = 'sources.information'
+
+    name = fields.Char(string='URL')
+
+class Section(models.Model):
+    _name = 'section.section'
+
+    name = fields.Char(string='Section Name')
